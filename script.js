9c78e485ae72a405273fff7eaa90da107d71a477
caitlyn <cbwiley4@icloud.com>
2026-09-18 13:52:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 
 
-const songs = ['go', 'silvera', 'nabakov', 'rearviewmirror', 'whole', 'pynch'];
+const songs = ['go', 'silvera', 'nabakov'];
 let songIndex = 2;
 
 
